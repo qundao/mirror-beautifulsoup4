f71ad1549bdb38701834544952d8352d2c3bf641
Leonard Richardson <leonardr@segfault.org>
2016-08-02 22:40:11 -0400

    Bump version number.

--- bs4/__init__.py
+++ bs4/__init__.py
@@ -21,7 +21,7 @@ http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 # found in the LICENSE file.
 
 __author__ = "Leonard Richardson (leonardr@segfault.org)"
-__version__ = "4.5.0"
+__version__ = "4.5.1"
 __copyright__ = "Copyright (c) 2004-2016 Leonard Richardson"
 __license__ = "MIT"
 
