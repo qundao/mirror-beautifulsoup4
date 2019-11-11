68e5565dd1be82b0f3e981abd8b5419f9d8258b8
Leonard Richardson <leonardr@segfault.org>
2019-11-11 13:51:41 -0500

    The html.parser tree builder now correctly handles DOCTYPEs that are   not uppercase. [bug=1848401]

--- bs4/builder/_htmlparser.py
+++ bs4/builder/_htmlparser.py
@@ -187,9 +187,10 @@ class BeautifulSoupHTMLParser(HTMLParser):
 
     def handle_decl(self, data):
         self.soup.endData()
-        if data.startswith("DOCTYPE "):
-            data = data[len("DOCTYPE "):]
-        elif data == 'DOCTYPE':
+        doctype_len = len("DOCTYPE ")
+        if data[:doctype_len].lower() == "doctype ":
+            data = data[doctype_len:]
+        elif len(data) == doctype_len-1 and data.lower() == 'doctype':
             # i.e. "<!DOCTYPE>"
             data = ''
         self.soup.handle_data(data)
