11eb35103a16bcfaccd91ce893ddf7f7adb5c0e1
Leonard Richardson <leonardr@segfault.org>
2023-04-11 11:23:31 -0400

    Added SoupStrainer examples from the documentation as unit tests, partly because I don't understand how some of the existing code works.

--- bs4/tests/test_strainer.py
+++ bs4/tests/test_strainer.py
@@ -410,3 +410,38 @@ class TestSoupStrainer(SoupTest):
             string=["Wrong string", "Also wrong", re.compile("string")]
         ).matches_tag(tag)
 
+    def test_documentation_examples(self):
+        """Medium-weight real-world tests based on the Beautiful Soup
+        documentation.
+        """
+        html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+        only_a_tags = SoupStrainer("a")
+        only_tags_with_id_link2 = SoupStrainer(id="link2")
+
+        def is_short_string(string):
+            return string is not None and len(string) < 10
+
+        only_short_strings = SoupStrainer(string=is_short_string)
+
+        a_soup = self.soup(html_doc, parse_only=only_a_tags)
+        assert ('<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a><a class="sister" href="http://example.com/lacie" id="link2">Lacie</a><a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>'
+                == a_soup.decode())
+
+        id_soup = self.soup(html_doc, parse_only=only_tags_with_id_link2)
+        assert (
+            '<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>'
+            == id_soup.decode()
+        )
+        string_soup = self.soup(html_doc, parse_only=only_short_strings)
+        assert '\n\n\nElsie,\nLacie and\nTillie\n...\n' == string_soup.decode()
